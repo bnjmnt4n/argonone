19bbee47f4cffd2dd318a1b3e817be03407dc613
Drew Risinger <drewrisinger@users.noreply.github.com>
2020-12-25 17:35:32 -0500

    packaging: add default.nix

---
+++ default.nix
@@ -0,0 +1,35 @@
+{ pkgs ? import <nixpkgs> {}}:
+
+with pkgs;
+let
+  # TODO: change
+  customPackages = pkgs.callPackage ../nixos-configuration/pkgs { };
+  inherit (customPackages.python3Packages) gpiozero smbus2;
+in
+python3Packages.buildPythonApplication {
+  pname = "argonone-rpi4";
+  version = lib.fileContents ./version.txt;
+  format = "pyproject";
+
+  src = lib.cleanSource ./.;
+
+  propagatedBuildInputs = [
+    python3Packages.click
+    gpiozero
+    smbus2
+    python3Packages.toml
+  ];
+
+  doSetuptoolsCheck = false;
+  installCheckPhase = ''
+    $out/bin/argononed.py --help
+    $out/bin/argononed_poweroff.py --help
+  '';
+
+  meta = with lib; {
+    description = "Argon One Service and Control Scripts for Raspberry Pi 4";
+    homepage = "https://github.com/drewrisinger/argonone";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ drewrisinger ];
+  };
+}
