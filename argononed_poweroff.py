b595715b2d758bb72349fb2977073205f7b5724d
Drew Risinger <drewrisinger@users.noreply.github.com>
2020-12-26 17:17:07 -0500

    argononed_poweroff: convert to click, gpiozero, smbus2

--- argononed_poweroff.py
+++ argononed_poweroff.py
@@ -2,24 +2,32 @@
 # -*- coding: utf-8 -*-
 # -*- mode: Python; tab-width: 4; indent-tabs-mode: nil; -*-
 # PEP 8, PEP 263.
-"""
-Argon Systemd Shutdown script
-"""
+"""Argon One (Raspberry Pi 4B) Systemd Shutdown script."""
+import click
+import gpiozero
+import smbus2
 
-import sys
-import smbus
-import RPi.GPIO as GPIO
 
-rev = GPIO.RPI_REVISION
-if rev == 2 or rev == 3:
-    bus = smbus.SMBus(1)
-else:
-    bus = smbus.SMBus(0)
+@click.command()
+@click.argument(
+    "shutdown_type",
+    default="poweroff",
+    type=click.Choice(["poweroff", "halt", "reboot", "kexec"], case_sensitive=False),
+)
+def cli(shutdown_type: str):
+    rev = int(gpiozero.pi_info().model[0])
+    if rev in {2, 3}:
+        bus = smbus2.SMBus(1)
+    else:
+        bus = smbus2.SMBus(0)
 
-if len(sys.argv) > 1:
-    bus.write_byte_data(0x1a,0,0)
-    if sys.argv[1] == "poweroff" or sys.argv[1] == "halt":
+    bus.write_byte_data(0x1A, 0, 0)
+    if shutdown_type.lower() in {"poweroff", "halt"}:
         try:
-            bus.write_byte_data(0x1a,0,0xFF)
+            bus.write_byte_data(0x1A, 0, 0xFF)
         except:
-            rev = 0
+            warnings.warn("Could not set LED OFF")
+
+
+if __name__ == "__main__":
+    cli()
